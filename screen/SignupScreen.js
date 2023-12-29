9968437a2a84ec18286c6e9c3df375428ae68f04
rajchatterz <iamraj.space@gmail.com>
2023-12-29 12:30:14 +0530

    done with the signupscreen

--- screen/SignupScreen.js
+++ screen/SignupScreen.js
@@ -1,28 +1,41 @@
 import { Image,ActivityIndicator, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View, Modal } from 'react-native'
 import React, { useState } from 'react'
 import background from '../assets/bcg.png'
+import { Svg,Path } from 'react-native-svg'
 const SignUpScreen = ({ navigation }) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
   const [emailError, setEmailError] = useState(false)
-  const [passwordError,setPasswordError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [showModal,setShowModal] = useState(false)
     const inputData = () => {
         if (name && password && email) {
           passDataToApi()
           setName('')
           setEmail('')
           setPassword('')
-          navigation.navigate('Login')
+          setShowModal(true)
+          
+          
         } else {
           errorHandling()
         }
     }
+  const login = () => {
+    navigation.navigate('Login')
+  }
+  const showDialoge = () => {
+    navigation.navigate('Login')
+    setShowModal(false)
+  }
+  
   const errorHandling = () => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     if (!email || !emailPattern.test(email)) {
       setEmailError(true)
-    } else if (!password || !password.length<9){
+    }
+    if (!password || !password.length < 9) {
       setPasswordError(true)
     }
     else {
@@ -44,18 +57,44 @@ const SignUpScreen = ({ navigation }) => {
       }
     }
   return (
-        <ImageBackground source={background} style={styles.imageBackground}>
+        <ImageBackground source={background} style={[styles.imageBackground,showModal&& styles.transparentBackground]}>
           <Image style={styles.cartImage} source={require('../assets/cart.png')} />
           <View style={styles.inputContainer}>
               <TextInput value={name} onChangeText={(text)=>setName(text)} placeholder='Name' style={styles.inputView}/>
-              <TextInput value={email} onChangeText={(text)=>setEmail(text)} placeholder='Email' style={styles.inputView}/>
-              <TextInput secureTextEntry={true} value={password} onChangeText={(text)=>setPassword(text)} placeholder='Password' style={styles.inputView}/>
-          </View>
+               <View style={styles.errorMessage}>
+                <TextInput value={email} onChangeText={(text) => setEmail(text)} placeholder='Email' style={styles.inputView} />
+                {emailError?<Text style={styles.errorText}>Please Enter a Valid Email</Text>:null}
+              </View>
+              <View style={styles.errorMessage}>
+                <TextInput secureTextEntry={true} value={password} onChangeText={(text) => setPassword(text)} placeholder='Password' style={styles.inputView} />
+                {passwordError?<Text style={styles.errorText}>Please Enter a valid password</Text>:null}
+              </View>
+      </View>
+      <View style={styles.login}>
+        <Text style={{fontWeight:'700',}}>Have an account</Text>
+        <TouchableOpacity onPress={()=>login()}>
+          <Text style={styles.logintext}>Log In</Text>
+        </TouchableOpacity>
+      </View>
           <View>
               <TouchableOpacity onPress={inputData} style={styles.buttonContainer}>
                   <Text style={styles.buttonText}>Sign Up</Text>
               </TouchableOpacity>
-          </View>
+      </View>
+      <Modal transparent={true} visible={showModal}>
+        <View style={styles.modalView}>
+          <Svg width={100} height={100} viewBox="0 0 50 50">
+            <Path
+              d="M25 2c11.05 0 20 8.954 20 20s-8.95 20-20 20S5 33.05 5 22 13.954 2 25 2zm10.94 15.236L21.617 33.7l-7.557-7.558a1.2 1.2 0 10-1.697 1.697l8.125 8.124a1.2 1.2 0 001.697 0l16.857-16.857a1.2 1.2 0 00-1.696-1.696z"
+              fill="#ffc637"
+            />
+          </Svg>
+          <Text style={{ color: '#1ef51ef9',fontSize:18,fontWeight:'800',marginBottom:20 }}>Data Saved Successfully</Text>
+          <TouchableOpacity onPress={()=>showDialoge()}>
+            <Text style={styles.modalText}>Click here</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
         </ImageBackground>
   )
 }
@@ -97,12 +136,58 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         // flex:1,
         justifyContent: 'center',
-        gap:20
+      gap: 20,
+        height:240
     },
     buttonText: {
         fontSize: 20,
         color:'#ffffff',
         fontWeight: '900',
         
-    },
+  },
+  errorMessage: {
+    height: 53
+    
+  },
+  errorText: {
+    color: '#d25454',
+    left: 10,
+    fontWeight: '600',
+    fontSize:12
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  modalText: {
+    backgroundColor: 'pink',
+    color: 'black',
+    paddingHorizontal: 60,
+    paddingVertical: 20,
+    borderRadius: 15,
+    color: '#fffafa',
+    fontWeight: '900',
+    fontSize:17
+  },
+  transparentBackground: {
+    opacity:0.2
+  },
+  login: {
+    flexDirection: 'row',
+   textAlign:'center',
+    // width: '90%',
+    left: 80,
+    width: 160,
+    marginTop:-20,
+    gap:10
+  },
+  logintext: {
+    fontWeight: '800',
+    fontSize: 13,
+    color: '#d25454',
+    justifyContent: 'center',
+    alignItems:'center'
+  }
 })
